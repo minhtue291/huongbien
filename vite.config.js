eac8c5ce1c0fa8493cb25b387101aca72ed5d081
minhtue510 <minhtue510@gmail.com>
2026-06-16 21:15:34 +0700

    first update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'POS Nhà Hàng',
+        short_name: 'POS',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
+
